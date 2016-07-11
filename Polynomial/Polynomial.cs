a79c8bac20e68d73e19a9a7e6a4374ac112789dc
Anastasiya Kostyukova <anastasiya.kostyukova.95@gmail.com>
2016-07-11 12:30:00 +0300

    first commit

---
+++ Polynomial/Polynomial.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Polynomial
+{
+  public class Polynomial : IFormattable
+  {
+    private double[] _coeffs;
+
+    public Polynomial(double[] coeffs)
+    {
+      _coeffs = coeffs;
+    }
+    
+    public double[] Coeffs
+    {
+      get
+      {
+        return _coeffs;
+      }
+    }
+
+    #region overrides of operators +, -
+
+    public static Polynomial operator +(Polynomial pol1, Polynomial pol2)
+    {
+      double[] resultCoeffs;
+      double[] minArray;
+
+      if (pol1.Coeffs.Length > pol2.Coeffs.Length)
+      {
+        resultCoeffs = new double[pol1.Coeffs.Length];
+        Array.Copy(pol1.Coeffs, resultCoeffs, pol1.Coeffs.Length);
+        minArray = pol2.Coeffs;
+      }
+      else
+      {
+        resultCoeffs = new double[pol2.Coeffs.Length];
+        Array.Copy(pol2.Coeffs, resultCoeffs, pol2.Coeffs.Length);
+        minArray = pol1.Coeffs;
+      }
+
+      var j = resultCoeffs.Length - 1;
+      for(var i = minArray.Length - 1; i >= 0; i--)
+      {
+        resultCoeffs[j] += minArray[i];
+        j--;
+      }
+
+      return new Polynomial(resultCoeffs);
+    }
+    
+    public static Polynomial operator -(Polynomial pol1, Polynomial pol2)
+    {
+      var tempPol2 = new double[pol2.Coeffs.Length];
+      Array.Copy(pol2.Coeffs, tempPol2, pol2.Coeffs.Length);
+      
+      for(var i = 0; i < tempPol2.Length; i++)
+      {
+        tempPol2[i] *= -1;
+      }
+
+      var tempPolinomial = new Polynomial(tempPol2);
+
+      return pol1 + tempPolinomial;
+    }
+
+    #endregion
+
+    #region all overrides of methods ToString()
+    public override string ToString()
+    {
+      return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public string ToString(string format)
+    {
+      return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public string ToString(IFormatProvider formatProvider)
+    {
+      return ToString("G", formatProvider);
+    }
+
+    public string ToString(string format, IFormatProvider formatProvider)
+    {
+      if(_coeffs == null)
+      {
+        throw new ArgumentException("Coefficients should be not null.");
+      }
+
+      if (String.IsNullOrEmpty(format))
+      {
+        format = "G";
+      }
+
+      if (formatProvider == null)
+      {
+        formatProvider = CultureInfo.CurrentCulture;
+      }
+
+      StringBuilder createdString = new StringBuilder();
+      int d = GetDegree();
+
+      for (int i = 0; i < d; i++)
+      {
+        var numberWithSign = WriteNumberWithSign(_coeffs[i], format, formatProvider, i == 0);
+
+        if (numberWithSign != null)
+        {
+          createdString.Append(numberWithSign);
+          createdString.Append("x");
+
+          if (d - i != 1)
+          {
+            createdString.Append("^");
+            createdString.Append(d - i);
+          }
+        }
+      }
+
+      createdString.Append(WriteNumberWithSign(_coeffs[_coeffs.Length - 1], format, formatProvider));
+
+      return createdString.ToString();
+    }
+    #endregion
+
+    public double CalculateValueOfPolynomial(double x)
+    {
+      double result = 0;
+      int d = GetDegree();
+
+      for (int i = 0; i <= GetDegree(); i++)
+      {
+        result += _coeffs[i] * Math.Pow(x, d);
+        d--;
+      }
+
+      return result;
+    }
+
+    #region private methods-helpers
+    private string WriteNumberWithSign(double a, string format, IFormatProvider formatProvider, bool isFirstElement = false)
+    {
+      StringBuilder temp = new StringBuilder();
+
+      if (a == 0)
+      {
+        return null;
+      }
+
+      if (isFirstElement == false)
+      {
+        if (a > 0)
+        {
+          temp.Append(" + ");
+        }
+        else
+        {
+          temp.Append(" - ");
+        }
+      }
+      else
+      {
+        if (a < 0)
+        {
+          temp.Append("- ");
+        }
+      }
+
+      if (Math.Abs(a) != 1)
+      {
+        temp.Append(Math.Abs(a).ToString(format, formatProvider));
+      }
+
+      return temp.ToString();
+    }
+
+    private int GetDegree()
+    {
+      if (_coeffs.Length == 0)
+      {
+        return 0;
+      }
+      return _coeffs.Length - 1;
+    }
+    #endregion
+  }
+}
---
+++ Task1Polynomial.Tests/Task1PolynomialTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1Polynomial;
+using NUnit.Framework;
+using System.Globalization;
+
+namespace Task1Polynomial.Tests
+{
+  public class Task1PolynomialTests
+  {
+    [Test]
+    [TestCase(new double[] { -1, 1.4, 0, 7.8, -2.2 }, ExpectedResult = "- x^4 + 1.4x^3 + 7.8x - 2.2")]
+    [TestCase(new double[] { 1, 0, -3.3 }, ExpectedResult = "x^2 - 3.3")]
+    [TestCase(new double[] { -1.2, 7.4, -1.9, -18.2 }, ExpectedResult = "- 1.2x^3 + 7.4x^2 - 1.9x - 18.2")]
+    [TestCase(new double[] { -1, -1.4, -1.1, +6, -7.8, 0 }, ExpectedResult = "- x^5 - 1.4x^4 - 1.1x^3 + 6x^2 - 7.8x")]
+    public string Polynomial_ToString(double[] coeffs)
+    {
+      Polynomial polynomial = new Polynomial(coeffs);
+      return polynomial.ToString();
+    }
+
+
+    [Test, TestCaseSource(nameof(FormatAndProviderData))]
+    public void Polynomial_ToStringFormattable(string format, IFormatProvider formatProvider, string expectedResult)
+    {
+      Polynomial polynomial = new Polynomial(new double[] { -1, -1.41, -1.1, +6, -7.87, 0 });
+      Assert.AreEqual(polynomial.ToString(format, formatProvider), expectedResult);
+    }
+
+    private static readonly object[] FormatAndProviderData =
+    {
+            new object[] {"G", CultureInfo.CurrentCulture, "- x^5 - 1.41x^4 - 1.1x^3 + 6x^2 - 7.87x"},
+            new object[] {"G", CultureInfo.GetCultureInfo("EN-us"), "- x^5 - 1.41x^4 - 1.1x^3 + 6x^2 - 7.87x" },
+            new object[] {"G", CultureInfo.GetCultureInfo("Ru-ru"), "- x^5 - 1,41x^4 - 1,1x^3 + 6x^2 - 7,87x" },
+            new object[] {"G", CultureInfo.GetCultureInfo("fr-FR"), "- x^5 - 1,41x^4 - 1,1x^3 + 6x^2 - 7,87x" },
+            new object[] {"F3", CultureInfo.GetCultureInfo("en-US"), "- x^5 - 1.410x^4 - 1.100x^3 + 6.000x^2 - 7.870x"},
+            new object[] {"F1", CultureInfo.InvariantCulture, "- x^5 - 1.4x^4 - 1.1x^3 + 6.0x^2 - 7.9x"},
+        };
+
+    [TestCase(new double[] { 8.0, -2.5, 4.5 }, 1.0, ExpectedResult = 10)]
+    [TestCase(new double[] { 1.7, 0, 1.8 }, 0, ExpectedResult = 1.8)]
+    [TestCase(new double[] { -1.2, 7.4, -5.9 }, 3.3, ExpectedResult = 5.452)]
+    public double Polynomial_GetValue(double[] coeffs, double x)
+    {
+      Polynomial polynomial = new Polynomial(coeffs);
+      return polynomial.CalculateValueOfPolynomial(x);
+    }
+
+
+    [TestCase(new double[] { 5.4, 0 }, new double[] { 7.2, -1.2, -8, 0 }, ExpectedResult = "7.2x^3 - 1.2x^2 - 2.6x")]
+    [TestCase(new double[] { -1, 1.1, 4.4}, new double[] { 1.2, -2, 1.1 }, ExpectedResult = "0.2x^2 - 0.9x + 5.5")]
+    [TestCase(new double[] { 7.2, -1.2, -8, 0 }, new double[] { -7.2, 1.2, 8, 0 }, ExpectedResult = "")]
+    public string Polynomial_Addition(double[] pol1, double[] pol2)
+    {
+      Polynomial firstPolynomial = new Polynomial(pol1);
+      Polynomial secondPolynomial = new Polynomial(pol2);
+
+      Polynomial resultPolynomial = firstPolynomial + secondPolynomial;
+      return resultPolynomial.ToString();
+    }
+
+    [TestCase(new double[] { -1, 1.1, 4.4 }, new double[] { -1, 1.1, 4.4 }, ExpectedResult = "")]
+    [TestCase(new double[] { 5.4, 0 }, new double[] { 7.2, -1.2, -8, 0.8 }, ExpectedResult = "- 7.2x^3 + 1.2x^2 + 13.4x - 0.8")]
+    public string Polynomial_Deduction(double[] firstCoeffs, double[] secondCoeffs)
+    {
+      Polynomial firstPolynomial = new Polynomial(firstCoeffs);
+      Polynomial secondPolynomial = new Polynomial(secondCoeffs);
+
+      Polynomial resultPolynomial = firstPolynomial - secondPolynomial;
+      return resultPolynomial.ToString();
+    }
+
+  }
+}
